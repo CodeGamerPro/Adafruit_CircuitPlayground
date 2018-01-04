80a8f68c122b3b51774a0b7e536f5238efcc0267
dean <deanm1278@gmail.com>
2018-01-04 14:26:14 -0500

    DM: updated vu meter

--- examples/vu_meter/vu_meter.ino
+++ examples/vu_meter/vu_meter.ino
@@ -22,23 +22,20 @@ Based on code for the adjustable sensitivity version of amplitie from:
 #include <Adafruit_CircuitPlayground.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <math.h>
 
-
-#define MIC_PIN         A4  // Microphone is attached to this analog pin (A4 for circuit playground)
 #define SAMPLE_WINDOW   10  // Sample window for average level
 #define PEAK_HANG       24  // Time of pause before peak dot falls
 #define PEAK_FALL        4  // Rate of falling peak dot
-#define INPUT_FLOOR     10  // Lower range of analogRead input
-#define INPUT_CEILING  500  // Max range of analogRead input, the lower the value the more sensitive (1023 = max)
 
+#define INPUT_FLOOR     56  // Lower range of mic sensitivity
+#define INPUT_CEILING  110  // Max range of analogRead input, the lower the value the more sensitive (1023 = max)
 
 byte peak = 16;        // Peak level of column; used for falling dots
 unsigned int sample;
 byte dotCount = 0;     //Frame counter for peak dot
 byte dotHangCount = 0; //Frame counter for holding peak dot
 
-float fscale(float originalMin, float originalMax, float newBegin, float newEnd, float inputValue, float curve);
+float mapf(float x, float in_min, float in_max, float out_min, float out_max);
 
 void setup() 
 {
@@ -48,30 +45,14 @@ void setup()
 void loop() 
 {
   int numPixels = CircuitPlayground.strip.numPixels();
-  unsigned long startMillis= millis();  // Start of sample window
   float peakToPeak = 0;   // peak-to-peak level
-
-  unsigned int signalMax = 0;
-  unsigned int signalMin = 1023;
   unsigned int c, y;
 
-  // collect data for length of sample window (in mS)
-  while (millis() - startMillis < SAMPLE_WINDOW)
-  {
-    sample = analogRead(MIC_PIN);
-    if (sample < 1024)  // toss out spurious readings
-    {
-      if (sample > signalMax)
-      {
-        signalMax = sample;  // save just the max levels
-      }
-      else if (sample < signalMin)
-      {
-        signalMin = sample;  // save just the min levels
-      }
-    }
-  }
-  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+  //get peak sound pressure level over the sample window
+  peakToPeak = CircuitPlayground.mic.soundPressureLevel(SAMPLE_WINDOW);
+
+  //limit to the floor value
+  peakToPeak = max(INPUT_FLOOR, peakToPeak);
  
   // Serial.println(peakToPeak);
 
@@ -80,8 +61,7 @@ void loop()
     CircuitPlayground.strip.setPixelColor(i,Wheel(map(i,0,numPixels-1,30,150)));
   }
 
-  //Scale the input logarithmically instead of linearly
-  c = fscale(INPUT_FLOOR, INPUT_CEILING, numPixels, 0, peakToPeak, 2);
+  c = mapf(peakToPeak, INPUT_FLOOR, INPUT_CEILING, numPixels, 0);
 
   // Turn off pixels that are below volume threshold.
   if(c < peak) {
@@ -122,72 +102,10 @@ void drawLine(uint8_t from, uint8_t to, uint32_t c) {
   }
 }
 
-
-float fscale( float originalMin, float originalMax, float newBegin, float
-newEnd, float inputValue, float curve){
-
-  float OriginalRange = 0;
-  float NewRange = 0;
-  float zeroRefCurVal = 0;
-  float normalizedCurVal = 0;
-  float rangedValue = 0;
-  boolean invFlag = 0;
-
-
-  // condition curve parameter
-  // limit range
-
-  if (curve > 10) curve = 10;
-  if (curve < -10) curve = -10;
-
-  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output 
-  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
-
-  /*
-   Serial.println(curve * 100, DEC);   // multply by 100 to preserve resolution  
-   Serial.println(); 
-   */
-
-  // Check for out of range inputValues
-  if (inputValue < originalMin) {
-    inputValue = originalMin;
-  }
-  if (inputValue > originalMax) {
-    inputValue = originalMax;
-  }
-
-  // Zero Refference the values
-  OriginalRange = originalMax - originalMin;
-
-  if (newEnd > newBegin){ 
-    NewRange = newEnd - newBegin;
-  }
-  else
-  {
-    NewRange = newBegin - newEnd; 
-    invFlag = 1;
-  }
-
-  zeroRefCurVal = inputValue - originalMin;
-  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
-
-  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine 
-  if (originalMin > originalMax ) {
-    return 0;
-  }
-
-  if (invFlag == 0){
-    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
-
-  }
-  else     // invert the ranges
-  {   
-    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange); 
-  }
-
-  return rangedValue;
+float mapf(float x, float in_min, float in_max, float out_min, float out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
